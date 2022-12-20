7107bd9ce394bde7fd8c37559d1f395c4a6884e3
palomac1 <paloma.dcastroleite@gmail.com>
2022-12-20 18:31:09 -0300

    calculadora

---
+++ app.js
@@ -0,0 +1,35 @@
+var calculadora = require("./calculadora");
+
+console.log(calculadora.mult(10,20));
+console.log(calculadora.soma(10,20));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var mostrarSite = true;
+const site = "www.guiadoprogramador.com";
+
+console.log("Hello World");
+console.log("Meu nome é Benjamin");
+
+if(mostrarSite){ // Se mostrar site
+    console.log(site); // Printe o site
+} */
+
+ 
